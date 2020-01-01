afd37297ce79d676b358b769bb9b2d3587b16846
Ethan Joseph <ethanrj91@gmail.com>
2019-12-31 20:10:51 -0800

    initial commit, added files

---
+++ DrawerMenu/DrawerMenu.h
@@ -0,0 +1,19 @@
+//
+//  DrawerMenu.h
+//  DrawerMenu
+//
+//  Created by Ethan Joseph on 12/27/19.
+//  Copyright © 2019 EJDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DrawerMenu.
+FOUNDATION_EXPORT double DrawerMenuVersionNumber;
+
+//! Project version string for DrawerMenu.
+FOUNDATION_EXPORT const unsigned char DrawerMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawerMenu/PublicHeader.h>
+
+
